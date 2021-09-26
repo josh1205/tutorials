a6f0ee72a2bf2b424c1006ce1cd025edc1b4d232
josh1205 <joshlavieri@gmail.com>
2021-09-26 17:32:25 -0400

    Finished materialUI course

--- materialUI/netninja/src/pages/Notes.js
+++ materialUI/netninja/src/pages/Notes.js
@@ -5,6 +5,7 @@ import { Grid } from '@material-ui/core'
 //import { Paper } from '@material-ui/core'
 import { Container } from '@material-ui/core'
 import NoteCard from '../components/NoteCard'
+import Masonry from 'react-masonry-css'
 
 export default function Notes() {
   const [notes, setNotes] = useState([])
@@ -23,17 +24,28 @@ export default function Notes() {
 
     const newNotes = notes.filter(note => note.id !== id)
     setNotes(newNotes)
+
+  }
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
   }
 
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {notes.map(note => (
-          <Grid key={note.id} xs={12} sm={6} md={4}>
+          <div key={note.ids}>
             <NoteCard note={note} handleDelete={handleDelete}/>
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
+
     </Container>
   )
 }
